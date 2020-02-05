b334c8cebd70dc4a899247482d32cfbdc7e009a0
Luis Olarte <luis.olarte@datatools.com.co>
2020-02-05 16:40:25 -0500

    First commit

---
+++ transporteWar/src/main/java/co/com/datatools/transporte/web/EmpresaBean.java
@@ -0,0 +1,24 @@
+package co.com.datatools.transporte.web;
+
+public class EmpresaBean {
+
+	private co.com.datatools.transporte.dto.Empresa empresa;
+
+	public EmpresaBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public co.com.datatools.transporte.dto.Empresa getEmpresa() {
+		return empresa;
+	}
+
+
+	
+	public void setEmpresa(co.com.datatools.transporte.dto.Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+
+
+}
